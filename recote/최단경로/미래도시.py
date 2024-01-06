b4db8731616aa2d53d567fbc48a51f6795f6c879
3zzinup <wintiger98@gmail.com>
2024-01-07 00:28:01 +0900

    123

--- recote/최단경로/미래도시.py
+++ recote/최단경로/미래도시.py
@@ -1,4 +1,3 @@
-# 미래도시
 INF = int(1e9)
 n, m = map(int, input().split())
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
@@ -18,7 +17,7 @@ for k in range(1, n + 1):
     for a in range(1, n + 1):
         for b in range(1, n + 1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
+3
 distance = graph[1][k] + graph[k][x]
 
 if distance >= INF:
