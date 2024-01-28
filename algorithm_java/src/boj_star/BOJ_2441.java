3e50864cb92b5b520bcc604a1eedd82c54d98e0d
김인엽 <inyub98@gmail.com>
2024-01-28 18:21:33 +0900

    asdas

---
+++ algorithm_java/src/boj_star/BOJ_2441.java
@@ -0,0 +1,21 @@
+package boj_star;
+
+import java.util.Scanner;
+
+public class BOJ_2441 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for(int i=n; i>0; i--) {
+      for(int j=0; j<n-i; j++) {
+        System.out.print(" ");
+      }
+      for(int j=i; j>0; j--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
