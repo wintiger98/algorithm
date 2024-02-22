a54e7812f523185c3c33cfe9708ba079ac4ace2d
김인엽 <inyub98@gmail.com>
2024-02-22 22:41:09 +0900

    :sparkles: MST 업로드

---
+++ algorithm_java/src/MST_Test.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class MST_Test {
+  static class Edge implements Comparator<Edge> {
+    int from, to, weight;
+
+    public Edge(int from, int to, int weight) {
+      this.from = from;
+      this.to = to;
+      this.weight = weight;
+    }
+
+    @Override
+    public int compare(Edge o1, Edge o2) {
+      return Integer.compare(o1.weight, o2.weight);
+    }
+  }
+  static Edge[] edgeList;
+  static int[] parents;
+  static int V;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    V = Integer.parseInt(st.nextToken());
+    int E = Integer.parseInt(st.nextToken());
+    edgeList = new Edge[E];
+
+    for(int i=0; i<E; i++) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+      int weight = Integer.parseInt(st.nextToken());
+      edgeList[i] = new Edge(from, to, weight);
+    } //  간선 리스트 생성
+
+    // 전처리 : 간선리스트 오름차순 정렬
+    Arrays.sort(edgeList);
+
+    parents = new int[V]; // 자신의 부모나 루트 저장(경로 압축으로 인해)
+    // 1. make - set
+    make();
+
+    // 2. 정렬된 간선 하나씩 꺼내어 신장트리 생성
+    int weight = 0;
+    int cnt = 0;
+    for(Edge edge : edgeList) {
+      if(!union(edge.from, edge.to)) continue; // 싸이클 발생
+      weight += edge.weight;
+      if(++cnt == V-1) break; // V-1개 선택 완료 => 종료
+    }
+
+    System.out.println(weight);
+  }
+  public static void make() {
+    for(int i=0; i<V; i++) {
+      parents[i] = i; // 모든 정점을 자신을 대표자(루트)로
+    }
+  }
+
+  public static int find(int a) { // a가 속한 트리의 루트 찾기
+    if(a == parents[a]) return a;
+    else return parents[a] = find(parents[a]); // 경로 압축
+  }
+
+  public static boolean union(int a, int b) {
+    int aRoot = find(a);
+    int bRoot = find(b);
+    if(aRoot == bRoot) return false; // a, b가 같은 트리에 속해있음 => union 불필요
+
+    parents[bRoot] = aRoot;
+    return true;
+  }
+}
