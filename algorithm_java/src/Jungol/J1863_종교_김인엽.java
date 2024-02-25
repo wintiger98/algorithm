b5a7958adf0e83e0bcbe70fecc7b029cb450b5ee
김인엽 <inyub98@gmail.com>
2024-02-25 23:46:44 +0900

    :sparkles: 문제 풀이 업로드

---
+++ algorithm_java/src/Jungol/J1863_종교_김인엽.java
@@ -0,0 +1,53 @@
+package Jungol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class J1863_종교_김인엽 {
+  static int N, M;
+  static boolean[] visited;
+  static int[] parents;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    parents = new int[N+1];
+    make();
+
+    int answer = N;
+    for(int i=0; i<M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      if(union(a, b)) answer--;
+    }
+
+    System.out.println(answer);
+  }
+
+  private static void make() {
+    for(int i=1; i<parents.length; i++) {
+      parents[i] = i;
+    }
+  }
+  private static int find(int x) {
+    if(x == parents[x]) return x;
+    return parents[x] = find(parents[x]);
+  }
+
+  private static boolean union(int a, int b) {
+    int aRoot = find(a);
+    int bRoot = find(b);
+    if(aRoot == bRoot) return false;
+
+    parents[bRoot] = aRoot;
+    return true;
+  }
+}
