393a61faacf452d5e6660f1d9c4423395b08d12a
김인엽 <inyub98@gmail.com>
2024-03-30 23:53:28 +0900

    :sparkles: 문제 풀이 업로드

--- algorithm_java/src/SWEA/S5656_벽돌깨기_김인엽.java
+++ algorithm_java/src/SWEA/S5656_벽돌깨기_김인엽.java
@@ -21,10 +21,10 @@ public class S5656_벽돌깨기_김인엽 {
             answer = Integer.MAX_VALUE;
 
             st = new StringTokenizer(br.readLine());
-            K = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            arr = new int[N][M];
+            K = Integer.parseInt(st.nextToken()); // 횟수
+            M = Integer.parseInt(st.nextToken()); // 열
+            N = Integer.parseInt(st.nextToken()); // 행
+            arr = new int[N][M]; // 맵
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -38,7 +38,7 @@ public class S5656_벽돌깨기_김인엽 {
         bw.close();
     }
 
-    static void print(int[][] arr) {
+    static void print(int[][] arr) { // 디버깅용 프린트 함수
         System.out.println();
         for (int[] a : arr) {
             System.out.println(Arrays.toString(a));
@@ -46,9 +46,9 @@ public class S5656_벽돌깨기_김인엽 {
         System.out.println();
     }
 
-    static void dfs(int[][] arr, int brickCnt) {
+    static void dfs(int[][] arr, int brickCnt) { // 모든 열에 대해 폭탄 떨궈보는 dfs 함수
         if (brickCnt == K) { // K개 다 했으면,
-            answer = Math.min(answer, calcRemoved(arr));
+            answer = Math.min(answer, calcRemoved(arr)); // 남아있는 폭탄이 가장 적음 -> 가장 많이 터트림
             return;
         }
 
@@ -57,7 +57,7 @@ public class S5656_벽돌깨기_김인엽 {
         }
     }
 
-    private static int calcRemoved(int[][] arr) {
+    private static int calcRemoved(int[][] arr) { // 남아있는 폭탄 개수 구하기
         int cnt = 0;
         for(int j=0; j<M; j++) {
             for(int i=N-1; i>=0; i--) {
@@ -68,8 +68,8 @@ public class S5656_벽돌깨기_김인엽 {
         return cnt;
     }
 
-    static int[][] bomb(int[][] arr, int w) {
-        int[][] newArr = new int[N][M];
+    static int[][] bomb(int[][] arr, int w) { // 실제로 터트리는 함수 부분
+        int[][] newArr = new int[N][M]; // arr 복사
         int x = -1;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -79,15 +79,15 @@ public class S5656_벽돌깨기_김인엽 {
                 }
             }
         }
-        if(x == -1) return newArr;
+        if(x == -1) return newArr; // 만약 w열에 대해 모든 행이 0 이면 터트릴게 없으니 끝
         int power = newArr[x][w];
         newArr[x][w] = 0;
-        shoot(newArr, x, w, power);
-        fallDown(newArr);
+        shoot(newArr, x, w, power); // 쭉 연쇄 폭발하기
+        fallDown(newArr); // 위에있는 거 아래로 땡기기
         return newArr;
     }
 
-    private static void fallDown(int[][] arr) {
+    private static void fallDown(int[][] arr) { // 위에있는거 아래로 땡기는 함수
         for (int i = 0; i < M; i++) { // 왼쪽부터
             for (int j = N - 1; j >= 0; j--) { // 아래쪽부터 처리하기
                 if (arr[j][i] != 0) {
@@ -107,12 +107,12 @@ public class S5656_벽돌깨기_김인엽 {
         }
     }
 
-    static void shoot(int[][] arr, int x, int y, int power) {
-        if (power <= 1) {
+    static void shoot(int[][] arr, int x, int y, int power) { // 연쇄폭발해버리기
+        if (power <= 1) { // 1이하는 영향을 안주니 return
             return;
         }
 
-        for (int p = 1; p < power; p++) {
+        for (int p = 1; p < power; p++) { // power -1 까지 폭발범위
             for (int i = 0; i < dx.length; i++) {
                 int cx = x + dx[i] * p;
                 int cy = y + dy[i] * p;
@@ -120,12 +120,12 @@ public class S5656_벽돌깨기_김인엽 {
                 if (cx < 0 || cx >= N || cy < 0 || cy >= M) {
                     continue;
                 }
-                if (arr[cx][cy] == 0) {
+                if (arr[cx][cy] == 0) { // 이미 터진건 끝
                     continue;
                 }
                 int tmpPower = arr[cx][cy];
-                arr[cx][cy] = 0;
-                shoot(arr, cx, cy, tmpPower);
+                arr[cx][cy] = 0; // 터트려버리기
+                shoot(arr, cx, cy, tmpPower); // 다음꺼 터트리기
             }
         }
     }
