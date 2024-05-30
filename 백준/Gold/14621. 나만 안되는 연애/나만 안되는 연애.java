d053fede1f91e20694d7cdf5289302c5c4eac2c2
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-05-30 23:38:00 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 204 ms, Memory: 18008 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
@@ -1,100 +1,102 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Edge {
-  int start;
-  int end;
-  int weight;
-
-  Edge(int start, int end, int weight) {
-    this.start = start;
-    this.end = end;
-    this.weight = weight;
-  }
-}
-
+/**
+ * MST인거 보고 풀었고, 구현방법도 기억안나서 싸피 때 배운 코드 보면서 했습니다,,
+ */
 public class Main {
-  static int[] parent;
-  static ArrayList<Edge> edgeList;
+  static class Edge implements Comparable<Edge> {
+    int from, to;
+    int weight;
+
+    public Edge(int from, int to, int weight) {
+      this.from = from;
+      this.to = to;
+      this.weight = weight;
+    }
+    @Override
+    public int compareTo(Edge o) {
+      return this.weight - o.weight;
+    }
+  }
+
+  static Edge[] edges; // 간선리스트
+  static int[] parents; // 부모 저장
+  static int N;
+  static boolean[] genders; // 성별
 
   public static void main(String[] args) throws NumberFormatException, IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int N = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
 
-    char[] university = new char[N + 1]; // 남초대학인지 여초대학인지 구분.
+    genders = new boolean[N]; // 남자면 true / 여자면 false
 
     st = new StringTokenizer(br.readLine());
-    for (int i = 1; i <= N; i++) {
-      university[i] = st.nextToken().charAt(0);
+    for (int i = 0; i < N; i++) {
+      genders[i] = st.nextToken().equals("M");
     }
 
-    edgeList = new ArrayList<>();
+    edges = new Edge[M]; // 간선 리스트
     for (int i = 0; i < M; i++) {
       st = new StringTokenizer(br.readLine());
 
-      int u = Integer.parseInt(st.nextToken());
-      int v = Integer.parseInt(st.nextToken());
-      int d = Integer.parseInt(st.nextToken());
-
-      edgeList.add(new Edge(u, v, d));
+      int from = Integer.parseInt(st.nextToken()) - 1;
+      int to = Integer.parseInt(st.nextToken()) - 1;
+      int weight = Integer.parseInt(st.nextToken());
+      edges[i] = new Edge(from, to, weight);
     }
+    // 간선리스트 오름차순 정렬
+    Arrays.sort(edges);
 
-    parent = new int[N + 1];
-    for (int i = 1; i <= N; i++) {
-      parent[i] = i;
+    parents = new int[N]; // 자신의 부모나 루트 저장
+
+    // 1. make set
+    make();
+
+    // 2. 정렬된 간선 하나씩 꺼내서 신장트리 생성
+    int weight = 0;
+    int cnt = 0;
+    for(Edge edge : edges) {
+      if(isSameGender(edge)) continue; // 같은 성이면 넘어가기
+      if(!union(edge.from, edge.to)) continue; // 싸이클 발생
+      weight += edge.weight;
+      if(++cnt == N-1) break; // N-1개 선택 완료 -> 종료
     }
+    if(cnt < N-1) weight = -1;
+    System.out.println(weight);
+  }
 
-    // 사심 경로를 기준으로 오름차순 정렬.
-    Collections.sort(edgeList, (e1, e2) -> e1.weight - e2.weight);
+  private static boolean isSameGender(Edge edge) {
+    return genders[edge.to] == genders[edge.from];
+  }
 
-    int cnt = 0, ans = 0;
-
-    // 크루스칼 알고리즘 수행.
-    for (int i = 0; i < edgeList.size(); i++) {
-      Edge edge = edgeList.get(i);
-
-      if (find(edge.start) != find(edge.end)) {
-        if (university[edge.start] != university[edge.end]) {
-          cnt++;
-          ans += edge.weight;
-
-          union(edge.start, edge.end);
-        }
-      }
+  public static void make() {
+    for(int i=0; i<N; i++) {
+      parents[i] = i;
     }
-
-    // N - 1개의 경로가 없으면 최소 신장 트리를 만들 수 없다는 의미임.
-    bw.write((cnt != N - 1 ? -1 : ans) + "\n");
-    bw.flush();
-    bw.close();
-    br.close();
   }
 
   public static int find(int x) {
-    if (x == parent[x]) {
+    if (x == parents[x]) {
       return x;
     }
-
-    return parent[x] = find(parent[x]);
+    return parents[x] = find(parents[x]);
   }
 
-  public static void union(int x, int y) {
-    x = find(x);
-    y = find(y);
+  public static boolean union(int x, int y) {
+    int xRoot = find(x);
+    int yRoot = find(y);
 
-    if (x != y) {
-      parent[y] = x;
-    }
+    if (xRoot == yRoot) return false; // 같은 트리임
+
+    parents[yRoot] = xRoot;
+    return true;
   }
 
 }
