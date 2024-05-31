59cc2a00311629021ef04ed5e90f468e660643fb
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-06-01 00:09:16 +0900

    [Silver III] Title: N과 M (3), Time: 620 ms, Memory: 179756 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -6,40 +6,35 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-  final static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
   static int N, M;
-  static int[] inputs, numbers;
-  static boolean[] selected;
+  static int[] numbers;
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
 
-    inputs = new int[N];
     numbers = new int[M];
-    selected = new boolean[N];
 
-    for(int i=1; i<=N; i++) inputs[i-1] = i;
-
-    perm(0);
+    permutation(0);
 
     bw.close();
   }
 
-  private static void perm(int cnt) throws IOException {
+  static void permutation(int cnt) throws IOException {
     if(cnt == M) {
-      for(int i=0; i<M; i++) {
-        bw.write(numbers[i] + " ");
+      for(int number : numbers) {
+        bw.write(number + " ");
       }
       bw.write("\n");
       return;
     }
-
-    for(int i=0; i<N; i++) {
-      numbers[cnt] = inputs[i];
-      perm(cnt+1);
+    for(int i=1; i<=N; i++) {
+      numbers[cnt] = i;
+      permutation(cnt+1);
     }
   }
+
 }
