cbfe34b2b910f8836322f92c8e1ff8d3ed142ac6
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-05-31 23:51:19 +0900

    [Silver III] Title: N과 M (3), Time: 664 ms, Memory: 296300 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  final static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  static int N, M;
+  static int[] inputs, numbers;
+  static boolean[] selected;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    inputs = new int[N];
+    numbers = new int[M];
+    selected = new boolean[N];
+
+    for(int i=1; i<=N; i++) inputs[i-1] = i;
+
+    perm(0);
+
+    bw.close();
+  }
+
+  private static void perm(int cnt) throws IOException {
+    if(cnt == M) {
+      for(int i=0; i<M; i++) {
+        bw.write(numbers[i] + " ");
+      }
+      bw.write("\n");
+      return;
+    }
+
+    for(int i=0; i<N; i++) {
+      numbers[cnt] = inputs[i];
+      perm(cnt+1);
+    }
+  }
+}
