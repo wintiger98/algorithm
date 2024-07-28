7ada8b3dfcda5ddcc71d98246452b8f4e0091b45
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-07-28 21:49:58 +0900

    [Gold III] Title: 사다리 조작, Time: 436 ms, Memory: 13976 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M, H;
     static boolean[][] ladder;
-    static int answer = 4;
-
+    static int answer;
+    static boolean isFinish;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,59 +18,55 @@ public class Main {
         M = Integer.parseInt(st.nextToken()); // 가로선 개수
         H = Integer.parseInt(st.nextToken()); // 세로선마다 가로선 가능 위치 개수
 
-        ladder = new boolean[H + 1][N + 1]; // 사다리 (인덱스가 1부터 시작이라서)
+        ladder = new boolean[H+1][N+1]; // 사다리 (인덱스가 1부터 시작이라서)
 
         // 가로선 정보
         for (int i = 0; i < M; i++) {
+            // b번 세로선과 b+1 세로선을 a번 점선 위치에서 연결함
+            // 1 <= a , 1 <= b
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             ladder[a][b] = true;
         }
 
-        // 가로선을 추가하는 DFS 시작
-        dfs(0, 1, 1);
-        System.out.println(answer > 3 ? -1 : answer);
+        for (int i = 0; i <= 3; i++) {
+            answer = i;
+            dfs(0, 1);
+            if(isFinish) break;
+        }
+        System.out.println(isFinish ? answer : -1);
     }
 
-    public static void dfs(int count, int x, int y) {
-        if (count >= answer) return; // 이미 현재 답보다 큰 경우 가지치기
-
-        if (countDown() == N) { // 모든 세로선이 올바르게 연결된 경우
-            answer = Math.min(answer, count);
+    public static void dfs(int count, int x) {
+        if(isFinish) return;
+        if(answer == count) {
+            if(check()) isFinish = true;
             return;
         }
 
-        for (int i = x; i <= H; i++, y = 1) {
-            for (int j = y; j < N; j++) {
-                if (!ladder[i][j] && !ladder[i][j - 1] && !ladder[i][j + 1]) {
+        for (int i = x; i < H+1; i++) {
+            for (int j = 1; j < N; j++) {
+                if(!ladder[i][j] && !ladder[i][j+1] && !ladder[i][j-1]) {
                     ladder[i][j] = true;
-                    dfs(count + 1, i, j + 2);
+                    dfs(count + 1, i);
                     ladder[i][j] = false;
                 }
             }
+
         }
     }
 
-    public static int countDown() {
-        int cnt = 0;
+    private static boolean check() {
         for (int i = 1; i <= N; i++) {
-            if (i == goDown(1, i)) {
-                cnt++;
+            int x = 1, y = i;
+            for (int j = 0; j < H; j++) {
+                if (ladder[x][y]) y++;
+                else if (ladder[x][y-1]) y--;
+                x++;
             }
+            if (y != i) return false;
         }
-        return cnt;
-    }
-
-    public static int goDown(int depth, int index) {
-        while (depth <= H) {
-            if (index < N && ladder[depth][index]) {
-                index++;
-            } else if (index > 1 && ladder[depth][index - 1]) {
-                index--;
-            }
-            depth++;
-        }
-        return index;
+        return true;
     }
 }
