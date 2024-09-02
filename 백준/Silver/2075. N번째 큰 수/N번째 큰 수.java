4cb3044fa23a595a833e60a21eec84fb70fcd85f
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-09-02 18:32:24 +0900

    [Silver II] Title: N번째 큰 수, Time: 736 ms, Memory: 261088 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -9,25 +11,31 @@ import java.util.StringTokenizer;
  * N 번째 큰 수 -> 위에서 N번째 수. 얘보다 큰 수는 N-1개 있음
  */
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); // 행렬 크기 & N번째 수
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>(
-			(o1, o2) -> o2 - o1
-		);
+		PriorityQueue<Integer> pq = new PriorityQueue<>(); // N 크기의 pq
 
 		StringTokenizer st;
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) {
-				pq.add(Integer.parseInt(st.nextToken()));
-			}
-		}
+				int num = Integer.parseInt(st.nextToken());
+				if(pq.size() < N){
+					pq.add(num);
+					continue;
+				}
 
-		for(int i = 0; i < N - 1; i++) {
-			pq.poll();
+				int min = pq.peek();
+				
+				if(num < min) { // 최소값보다 작으면 continue
+					continue;
+				} else { // 그 외에는 빼고 넣기
+					pq.poll();
+					pq.add(num);
+				}
+			}
 		}
 
 		System.out.println(pq.poll());
