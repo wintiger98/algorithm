a92c389c06de2ddde5d620330778cf84899f3bf1
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-11-01 19:39:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 108 ms, Memory: 12144 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,32 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[][] things = new int[N+1][2];
-        int[][] dp = new int[N+1][K+1];
-
-        for(int i=1; i<=N; i++) {
+        int[][] arr = new int[N + 1][2];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            things[i][0] = w;
-            things[i][1] = v;
+            arr[i][0] = w;
+            arr[i][1] = v;
         }
-        // 1 인덱스부터
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<K+1; j++) {
-                if(things[i][0] > j) {
-                    dp[i][j] = dp[i-1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], things[i][1] + dp[i-1][j-things[i][0]]);
-                }
+
+        int[] dp = new int[K + 1];
+        for (int i = 1; i <= N; i++) {
+            int weight = arr[i][0];
+            int value = arr[i][1];
+            for (int j = K; j >= weight; j--) {
+                dp[j] = Math.max(dp[j],
+                                 dp[j - weight] + value);
             }
         }
-        System.out.println(dp[N][K]);
+
+        System.out.println(dp[K]);
     }
 }
