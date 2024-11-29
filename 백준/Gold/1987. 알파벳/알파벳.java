04bc398c3b3bb2b3f891812c9017d0214bca29e3
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2024-11-29 23:27:08 +0900

    [Gold IV] Title: 알파벳, Time: 840 ms, Memory: 12140 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,61 +1,50 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int R, C; // 행 열
-    static char[][] board; // 보드
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1 ,0};
-
+    static int N, M;
+    static char[][] arr;
     static int answer;
-    static Map<Character, Integer> alphabet2Index = new HashMap<>();
+
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+
     public static void main(String[] args) throws IOException {
-        // 알파벳 -> 인덱스
-        String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for (int i = 0; i < 26; i++) {
-            alphabet2Index.put(alphabets.charAt(i), i);
-        }
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        arr = new char[N][M];
 
-        board = new char[R][C];
-
-        // 보드 받기
-        for (int i = 0; i < R; i++) {
-            String string = br.readLine();
-            for (int j = 0; j < C; j++) {
-                board[i][j] = string.charAt(j);
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = line.charAt(j);
             }
         }
 
-        // 알파벳 별 지났는지 여부
-        boolean[] visited = new boolean[26];
-        visited[alphabet2Index.get(board[0][0])] = true;
-        dfs(0, 0, visited, 1);
+        int flag = 0;
+        flag |= (1 << (arr[0][0] - 'A'));
+        dfs(0, 0, flag, 1);
 
         System.out.println(answer);
     }
 
-    static void dfs(int x, int y, boolean[] visited, int cnt) {
+    public static void dfs(int x, int y, int flag, int cnt) {
         answer = Math.max(answer, cnt);
+
         for (int i = 0; i < 4; i++) {
             int cx = x + dx[i];
             int cy = y + dy[i];
-            // 범위 체크
-            if(cx < 0 || cx >= R || cy < 0 || cy >= C) continue;
-            // 같은 알파벳 확인
-            if(visited[alphabet2Index.get(board[cx][cy])]) continue;
-            visited[alphabet2Index.get(board[cx][cy])] = true;
-            dfs(cx, cy, visited, cnt+1);
-            visited[alphabet2Index.get(board[cx][cy])] = false;
+
+            if(cx < 0 || cx >= N || cy < 0 || cy >= M) continue;
+
+            if((flag & (1 << (arr[cx][cy] - 'A'))) != 0) continue;
+
+            dfs(cx ,cy, flag | (1 << arr[cx][cy] - 'A'), cnt+1);
         }
     }
 }
