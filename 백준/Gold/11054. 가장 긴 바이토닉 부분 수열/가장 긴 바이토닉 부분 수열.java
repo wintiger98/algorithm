c0c707ea816a72f2c3023936abf71529dd2c0676
wintiger98 <78953151+wintiger98@users.noreply.github.com>
2025-01-22 18:03:00 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 244 ms, Memory: 18600 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,45 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int[] dpBig = new int[N];
+        int[] dpSmall = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int[] dpLeft = new int[N];
-		int[] dpRight = new int[N];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<i; j++) {
+                if(arr[j] < arr[i]) {
+                    dpBig[i] = Math.max(dpBig[i], dpBig[j] + 1);
+                }
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j])
-					dpLeft[i] = Math.max(dpLeft[j] + 1, dpLeft[i]);
-			}
-		}
-		for(int i=N-1; i>=0; i--) {
-			for(int j=N-1; j>i; j--) {
-				if (arr[i] > arr[j])
-					dpRight[i] = Math.max(dpRight[j] + 1, dpRight[i]);
-			}
-		}
-		
-//		System.out.println(Arrays.toString(dpLeft));
-//		System.out.println(Arrays.toString(dpRight));
+        for(int i=N-1; i>=0; i--) {
+            for(int j=N-1; j>i; j--) {
+                if(arr[j] < arr[i]) {
+                    dpSmall[i] = Math.max(dpSmall[i], dpSmall[j] + 1);
+                }
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			dpLeft[i] += dpRight[i];
-		}
-
-		System.out.println(Arrays.stream(dpLeft).max().orElseThrow(NoSuchElementException::new)+1);
-	}
+        for(int i=0; i<N; i++) {
+            dpBig[i] += dpSmall[i];
+        }
+        System.out.println(Arrays.stream(dpBig).max().orElse(0)+1);
+    }
 
 }
